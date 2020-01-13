9cfb5843484cda128c3d1a7cc895c14c4fd66bbb
Ajay Singh <a.pratap027@gmail.com>
2020-01-13 16:53:36 -0600

    Upadted working code

--- Money_Flow_index/money_fl_indx.py
+++ Money_Flow_index/money_fl_indx.py
@@ -13,36 +13,72 @@ def moneyFlowIndex(filename, n):
     url ='https://s3.amazonaws.com/istreet-questions-us-east-1/433997/sample.csv'
     df = pd.read_csv(url)
 
-    # Creating typical Price column with the calculated values
-    df['Typical Price'] = typicalPrice(df['High'], df['Low'], df['Close'])
-
-    
+    def typicalPrice(high, low, close):
+        return (high + low + close)/3
 
 
-def typicalPrice(high, low, close):
-    return (high + low + close)/3
-
-
-def moneyFlow():
+    # Creating Positive & Negative Money Flow
+    def moneyFlow():
 #     df.loc[0, 'Positive Money Flow'] = df.loc[0, 'Typical Price']
 #     df.loc[0, 'Negative Money Flow'] = df.loc[0, 'Typical Price']
-    df.loc[0, 'Positive Money Flow'] = 0
-    df.loc[0, 'Negative Money Flow'] = 0
+        df.loc[0, 'Positive Money Flow'] = 0
+        df.loc[0, 'Negative Money Flow'] = 0
+        
+        for i in range(1, len(df)):
+            if df.loc[i, 'Typical Price'] > df.loc[i-1, 'Typical Price']:
+                df.loc[i, 'Positive Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
+            else:
+                df.loc[i, 'Positive Money Flow'] = 0
+
+            if df.loc[i, 'Typical Price'] < df.loc[i-1, 'Typical Price']:
+                df.loc[i, 'Negative Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
+            else:
+                df.loc[i, 'Negative Money Flow'] = 0
+
+
+    # Creating Positive & Negative Money Flow
+    def moneyFlowSum(n):
+        # Positive money flow sum
+        for idx in range(n, len(df)):
+            df.loc[idx, 'Positive Money Flow Sum'] = sum(df['Positive Money Flow'][idx-n+1 : idx+1])
+        for ix in range(n):
+            df.loc[ix, 'Positive Money Flow Sum'] = 0
+
+        # Negative money flow sum
+        for idx in range(n, len(df)):
+            df.loc[idx, 'Negative Money Flow Sum'] = sum(df['Negative Money Flow'][idx-n+1 : idx+1])
+        for ix in range(n):
+            df.loc[ix, 'Negative Money Flow Sum'] = 0
+
+
+    # Creating Positive & Negative Money Flow
+    def mFlowIndex(n):
+        '''
+        Formula: --
+        Money ratio = PMFSum / NMFSum
+        Money Flow Index = (Money ratio * 100 )/ (1 + Money ratio)
+        '''
+        for i in range(n, len(df)):
+            moneyRatio = df.loc[i, 'Positive Money Flow Sum'] / df.loc[i, 'Negative Money Flow Sum'] 
+            df.loc[i, 'Money Flow Index'] = (moneyRatio * 100) / (1 + moneyRatio)
+        
+        for ix in range(n):
+            df.loc[ix, 'Money Flow Index'] = 0
+
+
+    # Function calling-----
+    # Creating typical Price column with the calculated values
+    df['Typical Price'] = typicalPrice(df['High'], df['Low'], df['Close'])
+    moneyFlow()
+    moneyFlowSum(n)
+    mFlowIndex(n)
+
+    # creating output csv file
+    newFile =  'money_flow_index_' + str(n) + '.csv'
+
+    return df.to_csv(newFile, sep=',', index=False)       
     
-    for i in range(1, len(df)):
-        if df.loc[i, 'Typical Price'] > df.loc[i-1, 'Typical Price']:
-            df.loc[i, 'Positive Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
-        else:
-            df.loc[i, 'Positive Money Flow'] = 0
-
-        if df.loc[i, 'Typical Price'] < df.loc[i-1, 'Typical Price']:
-            df.loc[i, 'Negative Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
-        else:
-            df.loc[i, 'Negative Money Flow'] = 0
-
-
-def MoneyFlowSum():
-    return None
+    
 
 
 if __name__ == '__main__':
