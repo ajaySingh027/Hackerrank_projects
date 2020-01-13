0cb69d3db414dbbf01bc8199dc54fa71115c0ccb
Ajay Singh <a.pratap027@gmail.com>
2020-01-13 15:53:07 -0600

    With folder vise

--- Money_Flow_index/money_fl_indx.py
+++ Money_Flow_index/money_fl_indx.py
@@ -23,21 +23,26 @@ def typicalPrice(high, low, close):
     return (high + low + close)/3
 
 
-def posMoneyFlow():
+def moneyFlow():
+#     df.loc[0, 'Positive Money Flow'] = df.loc[0, 'Typical Price']
+#     df.loc[0, 'Negative Money Flow'] = df.loc[0, 'Typical Price']
+    df.loc[0, 'Positive Money Flow'] = 0
+    df.loc[0, 'Negative Money Flow'] = 0
+    
+    for i in range(1, len(df)):
+        if df.loc[i, 'Typical Price'] > df.loc[i-1, 'Typical Price']:
+            df.loc[i, 'Positive Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
+        else:
+            df.loc[i, 'Positive Money Flow'] = 0
+
+        if df.loc[i, 'Typical Price'] < df.loc[i-1, 'Typical Price']:
+            df.loc[i, 'Negative Money Flow'] = df.loc[i, 'Typical Price'] * df.loc[i, 'Volume']
+        else:
+            df.loc[i, 'Negative Money Flow'] = 0
+
+
+def MoneyFlowSum():
     return None
-
-
-def negMoneyFlow():
-    return None
-
-
-def posMoneyFlowSum():
-    return None
-
-
-def negMoneyFlowSum():
-    pass
-
 
 
 if __name__ == '__main__':
