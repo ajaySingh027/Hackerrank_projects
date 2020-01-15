1a15fb486eae3df8d8e28ce8fcf2b5f1cfed3d8b
Ajay Singh <a.pratap027@gmail.com>
2020-01-15 02:05:17 -0600

    first version for Census attributes frequency code

---
+++ Census/census_attribFrequency.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+df = pd.read_csv('census.csv', header=None)
+print(df)
+
+def attributesSet(numberOfAttributes, supportThreshold):
+    pass
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    numberOfAttributes = int(input().strip())
+
+    supportThreshold = float(input().strip())
+
+    result = attributesSet(numberOfAttributes, supportThreshold)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
