530dd98ab3fd82de18ce17021f3fd956b59e0b50
Ajay Singh <a.pratap027@gmail.com>
2020-01-15 17:27:34 -0600

    attributeSet function code

--- Census/census_attribFrequency.py
+++ Census/census_attribFrequency.py
@@ -12,7 +12,14 @@ df = pd.read_csv('census.csv', header=None)
 print(df)
 
 def attributesSet(numberOfAttributes, supportThreshold):
-    pass
+    
+    # number of attributes in a combination
+    num = numberOfAttributes
+    
+    full_list = findCombinations(num)
+    finalList = finalSupport(full_list, supportThreshold)
+    
+    return finalList
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
